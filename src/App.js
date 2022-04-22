8f92a9221db40b0deeb4d9810c400d608ad8d5e9
Alex <thatsoalex@gmail.com>
2022-04-22 15:40:17 -0500

    Made Functionality for CreatePost + DisplayPosts + Post

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
-
+import DisplayPosts from './Components/DisplayPosts/DisplayPosts';
+import CreatePost from './Components/CreatePost/CreatePost';
+//import './App.css';
 
 function App() {
-  
- 
-  
+
+  const [posts, setPosts] = useState([{userName: "Alex Joosten", postBody: "To be defined."}])
+
+  function addNewPost(post){
+    let tempPosts = [post, ...posts]
+    setPosts(tempPosts)
+  }
+
   return (
-    <div>
-      <h3>Hello World</h3>
+    <div className="App">
+      Hello World!
+      <CreatePost addNewPost={addNewPost}/>
+      <DisplayPosts pagePosts={posts}/>
     </div>
   );
 }
-
-export default App;
